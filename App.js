baed497b68533664c8097af5140813d089589b57
DariaYashchuk <121041236+DariaYashchuk@users.noreply.github.com>
2023-09-12 14:25:41 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, View } from 'react-native'
+import { LoginScreen } from './src/Screens/LoginScreen.js'
+import { RegistrationScreen } from './src/Screens/RegistrationScreen.js'
+import { PostsScreen } from './src/Screens/PostsScreen.js'
+import { useFonts } from 'expo-font'
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
+    'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'),
+    'Inter-Medium': require('./assets/fonts/Inter-Medium.ttf')
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
+  return (
+    <View style={styles.container}>
+      <LoginScreen />
+      {/* <RegistrationScreen /> */}
+      {/* <PostsScreen /> */}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
